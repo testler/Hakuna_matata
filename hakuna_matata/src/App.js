d87fbe54867d3efa3af118a8155b161694cdb4d1
Koa Garces <koajgarces@yahoo.com>
2023-02-10 09:01:10 -0800

    navBar, picklocation, and landingPage updates

--- hakuna_matata/src/App.js
+++ hakuna_matata/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Routes, Route, useParams } from "react-router-dom";
 import Landing from "./pages/Landing/Landing";
 import Nav from "./components/Nav/Nav";
 import Feed from "./pages/Feed/Feed";
@@ -8,33 +9,23 @@ import Map from "./pages/Map/Map";
 import Profile from "./pages/Profile/Profile";
 import PickLocation from "./pages/PickLocation/PickLocation.jsx";
 function App() {
-  let [guestLogIn, setGuestLogIn] = false;
-  console.log(guestLogIn);
-
   //Empty array to add favorites to
   let favArr = [];
-
-  if (guestLogIn) {
-    document.getElementById("footer").style.display = "block";
-  }
 
   return (
     <div className="App">
       <div className="pages">
         <Routes>
           <Route index element={<Landing />} />
-          <Route
-            path="/pickLocation"
-            element={<PickLocation setGuestLogIn={setGuestLogIn} />}
-          />
+          <Route path="/pickLocation" element={<PickLocation />} />
           <Route path="/feed" element={<Feed favArr={favArr} />} />
           <Route path="/map" element={<Map />} />
           <Route path="/favorites" element={<Favorites favArr={favArr} />} />
           <Route path="/profile" element={<Profile />} />
         </Routes>
       </div>
-      <div className="navBox">
-        <Nav id="footer" />
+      <div id="navBox">
+        <Nav />
       </div>
     </div>
   );
--- hakuna_matata/src/pages/PickLocation/PickLocation.jsx
+++ hakuna_matata/src/pages/PickLocation/PickLocation.jsx
@@ -1,25 +1,60 @@
 import React from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
-
-const PickLocation = ({ setGuestLogIn }) => {
+import "./pickLocation.css";
+const PickLocation = () => {
   const toggleNav = () => {
-    setGuestLogIn(true);
+    document.getElementById("navBox").style.display = "flex";
   };
+
+  useEffect(() => {
+    document.getElementById("navBox").style.display = "none";
+  }, []);
 
   return (
     <div className="pickLocationPage">
       <h1 className="landingLogo">Eco-Pantry</h1>
-      <form>
-        <label>
-          Where are you located?
+      <form className="inputForm">
+        <div className="locateBox">
+          <p>Where are you located?</p>
           <input type="text" />
-        </label>
-        <label>
-          What are your food preferences?
-          <input type="text" />
-        </label>
+        </div>
+
+        <div className="options">
+          <p>What are your food preferences?</p>
+          <div className="checkBoxes">
+            <div className="checkBox">
+              <input type="checkbox" value="pescetarian" />
+              pescetarian
+            </div>
+            <div className="checkBox">
+              <input type="checkbox" value="gluten-free" />
+              gluten-free
+            </div>
+            <div className="checkBox">
+              <input type="checkbox" value="vegan" />
+              vegan
+            </div>
+            <div className="checkBox">
+              <input type="checkbox" value="kosher" />
+              kosher
+            </div>
+            <div className="checkBox">
+              <input type="checkbox" value="vegetarian" />
+              vegetarian
+            </div>
+            <div className="checkBox">
+              <input type="checkbox" value="dairy-free" />
+              dairy-free
+            </div>
+            <div className="checkBox">
+              <input type="checkbox" value="halal" />
+              halal
+            </div>
+          </div>
+        </div>
         <Link className="goButton" onClick={toggleNav} to={"/feed"}>
-          GO
+          See Offer Now
         </Link>
       </form>
     </div>
