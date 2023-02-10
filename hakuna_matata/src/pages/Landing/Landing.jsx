7ce6545b295f8888d8e949407190c13f2d361908
Koa Garces <koajgarces@yahoo.com>
2023-02-10 13:04:48 -0800

    added new styling to landing and location pages

--- hakuna_matata/src/pages/Landing/Landing.jsx
+++ hakuna_matata/src/pages/Landing/Landing.jsx
@@ -8,7 +8,12 @@ function Landing() {
   }, []);
   return (
     <div className="landingPage">
-      <h1 className="landingLogo">Eco-Pantry</h1>
+      {/* <h1 className="landingLogo">Eco-Pantry</h1> */}
+      <img
+        className="landingLogo"
+        alt="Eco-Pantry"
+        src={process.env.PUBLIC_URL + "/images/eco-pantry-logo.png"}
+      />
       <h1 className="welcome">Welcome Friends!</h1>
       <p className="about">
         Eco-Pantry is your friendly community food service! We alert you with
@@ -17,26 +22,24 @@ function Landing() {
       </p>
       <div className="wasteExamples">
         <div className="exampleBox">
-          <p className="exampleTitle">Food Wasted in the U.S.</p>
+          <p className="exampleTitle">Food Wasted in the U.S. last year</p>
           <div className="example" id="example1">
             <h2 className="exampleCount">130</h2>
             <p className="exampleDesc">Billion Meals</p>
           </div>
         </div>
         <div className="exampleBox">
-          <p className="exampleTitle">Meals Provided by Eco-Pantry</p>
+          <p className="exampleTitle2">Food Provided by Eco-Pantry last year</p>
           <div className="example" id="example2">
             <h2 className="exampleCount">200,000</h2>
             <p className="exampleDesc">Meals Sold</p>
           </div>
         </div>
       </div>
-
-      {/* <button className="startButton"> */}
       <Link className="startButton" to={`/pickLocation`}>
         Get Started
+        <img src={process.env.PUBLIC_URL + "/icon/arrow-right.svg"} />
       </Link>
-      {/* </button> */}
     </div>
   );
 }
